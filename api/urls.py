07990db8e8bd55ac634b18094106963602fb0912
t-nagasaka <66255777+t-nagasaka@users.noreply.github.com>
2021-10-17 18:38:14 +0900

    done feature/create_urls

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,12 @@ from . import views
 from rest_framework.routers import DefaultRouter
 
 
+router = DefaultRouter()
+router.register('user', views.UserViewSet)
+router.register('diary', views.DiaryViewSet)
+router.register('page', views.PageViewSet)
+
 urlpatterns = [
     path('register/', CreateUserView.as_view(), name='register'),
+    path('', include(router.urls)),
 ]
